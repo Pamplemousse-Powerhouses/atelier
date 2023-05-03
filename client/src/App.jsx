adaabbda94e249772b9e6fa18f22874673fe3f2b
Thang Nguyen <Thang14nguyen@gmail.com>
2023-05-03 14:41:13 -0700

    updating file structure to match workflow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import RatingsReviews from './RatingsReviews/RatingsReviews';
+import RatingsReviews from './RatingsReviews/index';
 
 export default function App() {
   return (
