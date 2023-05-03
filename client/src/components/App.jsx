ace9535b86742e3b538f241adf876c5af528a23e
Thang Nguyen <Thang14nguyen@gmail.com>
2023-05-03 11:10:41 -0700

    reverting change back to app.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import RatingsReviews from './RatingsReviews/RatingsReviews.jsx'
+import RatingsReviews from './RatingsReviews/RatingsReviews.jsx';
 
 export default function App() {
   return (
-  <div>
-    <h1> App </h1>
-    <h1> --- Ratings & Reviews Section --- </h1>
-    <div> < RatingsReviews /> </div>
-  </div>);
+    <div>
+      <h1> App </h1>
+      <h1> --- Ratings & Reviews Section --- </h1>
+      <div>
+        {' '}
+        <RatingsReviews />
+        {' '}
+      </div>
+    </div>
+  );
 }
 
 // ok
